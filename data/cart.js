bbd03d141b4e79ee356391dac36a9429ccc100a8
ofentse <ofentsebotlhale88@outlook.com>
2024-06-03 23:36:47 +0200

    Save daa and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,18 @@
+//import deliveryOptions from "../deliveryOptions";
+
 export let cart = JSON.parse(localStorage.getItem("cart"));
 if (!cart) {
   cart = [
-    { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 1 },
-    { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 },
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "2",
+    },
   ];
 }
 function saveToStorage() {
@@ -20,10 +30,12 @@ export function addToCart(productId) {
     matchingItem.quantity += 1;
   } else {
     cart.push({
-      productId: productId,
+    productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
+
   saveToStorage();
 }
 

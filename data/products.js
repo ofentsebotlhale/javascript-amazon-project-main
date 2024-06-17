20fc19eee9501cc8d4332b886435ede486da9422
ofentsebotlhale <151065301+ofentsebotlhale@users.noreply.github.com>
2024-06-17 00:00:04 +0000

    Added some notes on 'this' keyword and classes

--- data/products.js
+++ data/products.js
@@ -56,6 +56,31 @@ class Clothing extends Product {
   }
 }
 
+//Built-in Class
+//const date = new Date();
+//console.log(date.toLocaleTimeString());
+
+//this keyword
+//console.log(this);
+//const object2 = {
+//  a: 2,
+//  b: this.a,
+//};
+//function logThis() {
+//  console.log(this);
+//}
+//logThis();
+//call
+//logThis.call("hello");
+//logThis.apply("none");
+//this;
+//const object3 = {
+//  method: () => {
+//    console.log(this);
+//  },
+//};
+//object3.method();
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",

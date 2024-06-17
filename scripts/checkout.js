c6b1f2e6faf51ca198dc1ee046b2bc9727956ecd
ofentsebotlhale <151065301+ofentsebotlhale@users.noreply.github.com>
2024-06-17 08:42:11 +0000

    convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,46 @@ import renderPaymentSummary from "./checkout/paymentSummary.js";
 //import "../data/cart-class.js"
 //import "../data/backend-practice.js";
 import { loadProducts } from "../data/products.js";
-loadProducts(() => {
+import { loadCart } from "../data/cart.js";
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value2");
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+})
+  .then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .finally(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+
+//loadProducts(() => {
+//loadCart(() => {
+//renderOrderSummary();
+//renderPaymentSummary();
+//});
+//});*/
